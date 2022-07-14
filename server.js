71974ad53faa1f2f357c4c3e189b111a441fe802
yangxingkun <1612166244@qq.com>
2022-07-14 15:52:09 +0800

    初始文件

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+let users = [
+  {
+    id: 1,
+    name: "zhufeng",
+  },
+  {
+    id: 2,
+    name: "zhufeng2",
+  },
+  {
+    id: 3,
+    name: "zhufeng3",
+  },
+  {
+    id: 4,
+    name: "zhufeng4",
+  },
+];
+let server = http.createServer(function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (req.url == "/api/users") {
+    res.end(JSON.stringify(users));
+  }else{
+    res.end('not found')
+  }
+});
+
+server.listen(3000, (req, res) => {
+  console.log("服务器启动在3000");
+});
